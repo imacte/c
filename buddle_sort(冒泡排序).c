f54c7f150ee47d06f15402c3101ae23379557215
imacte <119027683+imacte@users.noreply.github.com>
2023-10-17 20:12:30 +0800

    Update buddle_sort(冒泡排序).c

--- buddle_sort(冒泡排序).c
+++ buddle_sort(冒泡排序).c
@@ -5,6 +5,7 @@ void bubble_sort(int arr[], int sz)
     int i = 0;
     for (i = 0; i < sz - 1; i++)
     {
+        int flag = 1;//假设这一趟要排序的数据已经有序
         int j = 0;
         for (j = 0; j < sz - i - 1; j++)
         {
@@ -13,7 +14,12 @@ void bubble_sort(int arr[], int sz)
                 int temp = arr[j];
                 arr[j] = arr[j + 1];
                 arr[j + 1] = temp;
+                flag = 0;//本趟排序的数据其实不完全有序
             }
+        }
+        if (flag == 1)
+        {
+            break;//break用于解除循环，if不是循环无法使用，但是这个break是给外面的for循环用的
         }
     }
 }
@@ -22,7 +28,9 @@ int main()
     int arr[] = { 9, 8, 7, 6, 5, 4, 3, 2, 1, 0 };
     int i = 0;
     int sz = sizeof(arr) / sizeof(arr[0]);
-    bubble_sort(arr,sz);
+    //对arr进行排序，排成升序
+    //arr是数组,我们对数组arr进行传参，实际上传递过去的是数组arr首元素的地址 &arr[0]
+    bubble_sort(arr,sz);//冒泡排序函数
     for (i = 0; i < sz; i++)
     {
         printf(" %d", arr[i]);
